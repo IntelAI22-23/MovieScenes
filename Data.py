c2c3672d2536960d1bf940c31b777b0e8a2bc3af
YashPame <88240082+YashPame@users.noreply.github.com>
2022-10-15 16:41:23 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -5,5 +5,11 @@ Data = [
         "image": "IASCLogo.png",
         "Context": "This is a collection of iconic movie scenes and posters.",
     },
+    {
+        "id": 1,
+        "Movie Name": "Baahubali: The Beginning",
+        "image": "https://flxt.tmsimg.com/assets/p11546593_p_v8_af.jpg"
+        "Context": "",
+    },
 
 ]
